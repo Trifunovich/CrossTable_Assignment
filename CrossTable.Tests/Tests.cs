844c593d869b026322dc6d0a327dada9b1b15fbb
Aleksandar Trifunovich <trifunovich.aleksandar@gmail.com>
2024-12-16 13:27:53 +0100

    Add project files.

---
+++ CrossTable.Tests/Tests.cs
@@ -0,0 +1,122 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace CrossTable.Tests {
+    [SetCulture("en-US")]
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void Test2x2() {
+            AssertCrossTable(
+@"Bananas,09/12/2021,20
+Apples,08/08/2020,10
+Bananas,07/25/2021,30
+Apples,05/07/2020,10
+Apples,01/02/2021,60
+Bananas,05/11/2020,40
+Bananas,02/22/2021,55
+Apples,02/04/2020,12",
+
+@"||2020|2021|
+|-|-|-|
+|Apples|$32.00|$60.00|
+|Bananas|$40.00|$105.00|
+");
+        }
+
+        [Test]
+        public void Test1x1() {
+            AssertCrossTable(
+@"Bananas,09/12/2021,20.17
+Bananas,07/25/2021,30.22",
+
+@"||2021|
+|-|-|
+|Bananas|$50.39|
+");
+        }
+
+        [Test]
+        public void Culture() {
+            CultureInfo culture = CultureInfo.CurrentCulture;
+            CultureInfo.CurrentCulture = new CultureInfo("es-ES");
+            try {
+                AssertCrossTable(
+@"Bananas,09/12/2021,20.17",
+
+@"||2021|
+|-|-|
+|Bananas|$20.17|
+");
+            } finally {
+                CultureInfo.CurrentCulture = culture;
+            }        
+        }
+
+        [Test]
+        public void EmptyCell() {
+            AssertCrossTable(
+@"Bananas,09/12/2020,20
+Apples,08/08/2021,10
+Apples,05/07/2020,10",
+
+@"||2020|2021|
+|-|-|-|
+|Apples|$10.00|$10.00|
+|Bananas|$20.00||
+");
+        }
+
+//        [Test]
+//        public void JsonGenerator() {
+//            AssertCrossTable((reader, writer) => {
+//                var generator = new JsonGenerator();
+//                generator.GenerateTable(reader, writer);
+//            },
+
+//@"Bananas,01/05/2021,10.0
+//Apples,02/09/2021,20.0
+//Apples,01/03/2021,11.5
+//Apples,07/04/2021,3
+//Bananas,02/02/2020,5.0",
+
+//@"{""Apples"":{""2021"":34.5},""Bananas"":{""2020"":5,""2021"":10}}"
+//            );
+
+//            AssertCrossTable((reader, writer) => {
+//                var generator = new MDGenerator();
+//                generator.GenerateTable(reader, writer);
+//            },
+
+//@"Bananas,01/05/2021,10.0
+//Apples,02/09/2021,20.0
+//Apples,01/03/2021,11.5
+//Apples,07/04/2021,3
+//Bananas,02/02/2020,5.0",
+
+//@"||2020|2021|
+//|-|-|-|
+//|Apples||$34.50|
+//|Bananas|$5.00|$10.00|
+//"
+//);
+//        }
+
+        [DebuggerStepThrough]
+        protected virtual void AssertCrossTable(string input, string output) {
+            //AssertCrossTable(Solution_Original.GenerateCrossTable, input, output);
+            AssertCrossTable(Solution.GenerateCrossTable, input, output);
+        }
+        [DebuggerStepThrough]
+        protected void AssertCrossTable(Action<TextReader, TextWriter> generate, string input, string output) {
+            using var reader = new StringReader(input);
+            var builder = new StringBuilder();
+            using var writer = new StringWriter(builder);
+            generate(reader, writer);
+            string result = builder.ToString();
+            Assert.AreEqual(output.Replace("\r\n", Environment.NewLine), result);
+        }
+    }
+}
---
+++ CrossTable/Solution.cs
@@ -0,0 +1,73 @@
+﻿using System.Buffers;
+using System.Collections.Generic;
+using System.Globalization;
+
+//Transforms csv input into md table. See tests project for sample input and output.
+public class Solution {
+    record Order(string Product, DateTime Date, float Price);
+
+    static readonly string cultureName = "en-US";
+    static readonly CultureInfo cultureInfo = new CultureInfo(cultureName);
+    
+    public static void GenerateCrossTable(TextReader reader, TextWriter writer) {
+        //Read lines with comma-separated values into a list of orders
+
+        var lines = new List<string>();
+
+        while (reader.Peek() >= 0)
+        {
+            var readCount = reader.ReadLine();
+            if (readCount?.Length <= 0) continue;
+            lines.Add(readCount);
+        }
+     
+        
+        List<Order> orders = new();
+        for(int i = 0; i < lines.Count(); i++) {
+            string[] pieces = lines[i].Split(",");
+            string name = pieces[0];
+            DateTime date = DateTime.Parse(pieces[1], cultureInfo);
+            float price = float.Parse(pieces[2], cultureInfo);
+            orders.Add(new Order(name, date, price));
+        }
+
+        //Calculate cross-table summary values
+        Dictionary<ValueTuple<string, int>, float> sums = new();
+        foreach(Order order in orders) {
+            var sumKey = ValueTuple.Create(order.Product, order.Date.Year);
+            if(sums.ContainsKey(sumKey))
+                sums[sumKey] += order.Price;
+            else
+                sums[sumKey] = order.Price;
+        }
+
+        //Calculate ordered lists of years and product names
+        var years = 
+            sums.
+                Keys.Select(x => x.Item2).Distinct().Order().ToArray();
+
+        var products = sums.
+            Keys.Select(x => x.Item1).Order().Distinct().ToArray();
+
+        //Produce resulting table
+        string header = GenerateTableLine(null, years.Select(x => x.ToString()));
+        string delimeter = GenerateTableLine("-", Enumerable.Repeat("-", years.Length));
+        List<string> rows = new();
+        foreach(var product in products)
+        {
+            var newCells = years
+                .Select(year =>
+                    sums.GetValueOrDefault(ValueTuple.Create(product, year), 0))
+                .Select(x => x == 0 ? string.Empty : x.ToString("c", cultureInfo));
+            
+            string row = GenerateTableLine(product, newCells.ToArray());
+            rows.Add(row);
+        }
+        writer.Write(header + delimeter + string.Concat(rows));
+    }
+    static string GenerateTableLine(string? left, IEnumerable<string> values) { 
+        return $"|{left}|{string.Join("|", values)}|{Environment.NewLine}";
+    }
+
+
+}
